0ae08d8d679fa943e311fd18d59c5a55fe8673ad
saurabh <77434364+Saurabhdaswant@users.noreply.github.com>
2025-02-27 14:18:10 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
           <Timer />
           <Transactions />
           <Screenshots />
-          <Wallet />
+          {/* <Wallet /> */}
         </div>
       </main>
     </div>
