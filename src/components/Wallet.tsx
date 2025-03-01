ad7dc5465c06ae0a97f799a89b0625e0cb08c355
saurabh <77434364+Saurabhdaswant@users.noreply.github.com>
2025-03-01 09:38:07 +0530

    second screen filling

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -3,6 +3,7 @@ import {
   CreditCardIcon,
   LockClosedIcon,
   PencilIcon,
+  ShieldCheckIcon,
 } from "@heroicons/react/16/solid";
 import { AnimatePresence, motion } from "framer-motion";
 import { EllipsisVertical } from "lucide-react";
@@ -207,6 +208,22 @@ const Wallet = () => {
                               background: "#57dc5c",
                             }}
                           />
+                          <motion.div
+                            className="flex items-center mt-4 justify-center px-12 gap-2 space-x-2"
+                            layout
+                            initial={{ opacity: 0, y: 5, scale: 0.95 }}
+                            animate={{ opacity: 1, y: 0, scale: 1 }}
+                            exit={{ opacity: 0, y: 0 }}
+                            transition={{ duration: 0.3 }}
+                            onClick={() => setScreen("third")}
+                          >
+                            <div className="flex-1 border-b border-gray-100/80" />
+                            <button className="inline-flex items-center gap-2 text-[13px] font-medium text-[#A6A6A6]">
+                              <PencilIcon className="w-3.5 h-3.5" />
+                              Input Manually
+                            </button>
+                            <div className="flex-1 border-b border-gray-100/80" />
+                          </motion.div>
                         </div>
                       )}
                       {screen === "third" && (
@@ -230,41 +247,40 @@ const Wallet = () => {
                         </div>
                       )}
                     </div>
-                    {screen === "second" && (
-                      <motion.div
-                        className="flex items-center mt-4 justify-center px-12 gap-2 space-x-2"
-                        layout
-                        initial={{ opacity: 0, y: 5, scale: 0.95 }}
-                        animate={{ opacity: 1, y: 0, scale: 1 }}
-                        exit={{ opacity: 0, y: 0 }}
-                        transition={{ duration: 0.3 }}
-                        onClick={() => setScreen("third")}
-                      >
-                        <div className="flex-1 border-b border-gray-100/80" />
-                        <button className="inline-flex items-center gap-2 text-[13px] font-medium text-[#A6A6A6]">
-                          <PencilIcon className="w-3.5 h-3.5" />
-                          Input Manually
-                        </button>
-                        <div className="flex-1 border-b border-gray-100/80" />
-                      </motion.div>
-                    )}
                   </>
                 )}
               </div>
               <div>
-                <div className="mt-10 mb-5 p-4 border-t-2 border-dashed border-gray-100 mx-auto w-[90%] rounded-3xl">
+                <div className=" relative mt-10 opacity-50 mb-5 p-4 pt-5 border-t-2 border-dashed border-gray-200 mx-auto w-[90%] rounded-3xl">
                   {screen === "second" ? (
-                    <motion.p
-                      className="text-[#A6A6A6] text-xs text-center"
-                      layout
-                      initial={{ opacity: 0, y: 5, scale: 0.95 }}
-                      animate={{ opacity: 1, y: 0, scale: 1 }}
-                      transition={{ duration: 0.3 }}
-                      exit={{ opacity: 0, y: 0 }}
-                    >
-                      After entering your Secret Recovery Phrase, you’ll be able
-                      to continue the importing process.
-                    </motion.p>
+                    <>
+                      <ShieldCheckIcon
+                        color="#A6A6A6"
+                        className=" absolute left-1/2 -translate-x-1/2 -top-3 w-5 h-5"
+                      />
+
+                      <motion.p
+                        className="text-[#A6A6A6] text-xs text-center"
+                        layout
+                        initial={{ opacity: 0, y: 5, scale: 0.95 }}
+                        animate={{ opacity: 1, y: 0, scale: 1 }}
+                        transition={{ duration: 0.3 }}
+                        exit={{ opacity: 0, y: 0 }}
+                      >
+                        After entering your Secret Recovery Phrase, you’ll be
+                        able to continue the importing process.
+                      </motion.p>
+
+                      <motion.button
+                        disabled={true}
+                        className="bg-green-500  w-full rounded-full text-white font-semibold py-2.5 px-4  mt-6"
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
+                        transition={{ duration: 0.2 }}
+                      >
+                        Import
+                      </motion.button>
+                    </>
                   ) : (
                     <p className="text-transparent text-xs text-center">
                       nonses importing proc importing procimporting
@@ -272,7 +288,7 @@ const Wallet = () => {
                     </p>
                   )}
                 </div>
-                <div className="bg-green-60 opacity-0 mt-5 rounded-full p-4 text-white w-[90%] mx-auto h-10" />
+                {/* <div className="bg-green-60 opacity-0 mt-5 rounded-full p-4 text-white w-[90%] mx-auto h-10" /> */}
               </div>
             </div>
           </div>
