a2099bdc80dcb98c9e988729f806c3d478629711
saurabh <77434364+Saurabhdaswant@users.noreply.github.com>
2025-03-02 17:20:29 +0530

    fix

--- src/components/WalletComp/Toptitles.tsx
+++ src/components/WalletComp/Toptitles.tsx
@@ -1,13 +1,33 @@
-import { swipeHorizontally } from "@/utils/constants";
-import { AnimatePresence, motion } from "framer-motion";
-import SecondTopTitles from "./SecondTopTitles";
 import { useWalletStep } from "@/providers/WalletStepProvider";
+import {
+  fromFirstAnimation,
+  fromOtherAnimation,
+  swipeHorizontally,
+} from "@/utils/constants";
+import { AnimatePresence, motion } from "framer-motion";
 export default function Toptitles() {
-  const { currentStep } = useWalletStep();
+  const { currentStep, prevStep, nextStep } = useWalletStep();
+
+  const animationProps =
+    prevStep === "first" || nextStep === "first"
+      ? fromFirstAnimation
+      : fromOtherAnimation;
 
   return (
     <AnimatePresence mode="sync">
-      {currentStep === "second" && <SecondTopTitles />}
+      {currentStep === "second" && (
+        <motion.div key="second1" className="p-8 absolute top-[108px]">
+          <motion.h2
+            {...animationProps}
+            className="  text-[#1E1E1E] font-semibold text-xl"
+          >
+            Import Wallet
+          </motion.h2>
+          <motion.p {...animationProps} className="text-[#A6A6A6] mt-1.5">
+            Enter your Secret Recovery Phrase to import your wallet securely.
+          </motion.p>
+        </motion.div>
+      )}
       {currentStep === "third" && (
         <motion.div key="second2" className="p-3 absolute top-[84px] w-full ">
           <motion.h2
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -21,3 +21,17 @@ export const swipeHorizontally = {
   exit: { opacity: 0, y: 0, x: 100 },
   transition: { duration: 0.3 },
 };
+
+export const fromFirstAnimation = {
+  initial: { opacity: 0, y: 10, x: 0, scale: 0.9 },
+  animate: { opacity: 1, y: 0, x: 0, scale: 1 },
+  exit: { opacity: 0, y: 10, x: 0, scale: 0.9 },
+  transition: { duration: 0.3 },
+};
+
+export const fromOtherAnimation = {
+  initial: { opacity: 0, y: 0, x: -100 },
+  animate: { opacity: 1, y: 0, x: 0, scale: 1 },
+  exit: { opacity: 0, y: 0, x: -100 },
+  transition: { duration: 0.3 },
+};
